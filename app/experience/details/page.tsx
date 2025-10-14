9d77f34bd7b1f0115a4e7f035315a472b91c007d
Wilson Ku <wilsoneku@gmail.com>
2025-10-13 18:17:38 -0700

    Proof of Concept

--- app/experience/details/page.tsx
+++ app/experience/details/page.tsx
@@ -20,9 +20,8 @@ export default function ExperienceDetailsPage() {
 
             <div className="mt-8 flex justify-center">
                 <Link href="/" className="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition-colors">
-                    Back to Search (TO BE IMPLEMENTED)
+                    Back to Search (TODO)
                 </Link>
             </div>
         </main>
     )
-}
