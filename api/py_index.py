a4e781ea8a5d12d92e9c51322ac19835464ddad0
Wilson Ku <wilsoneku@gmail.com>
2025-10-26 20:39:30 -0700

    added cors headers to index

--- api/py_index.py
+++ api/py_index.py
@@ -2,6 +2,13 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+### CORS headers
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
+    return response
+
 @app.route('/', methods=['GET'])
 @app.route('/index', methods=['GET'])
 def root():
