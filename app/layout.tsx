3fd46f981bde736d350679fe1746338ee205d2a3
Wilson Ku <wilsoneku@gmail.com>
2025-11-12 18:07:12 -0800

    added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,18 +14,18 @@ export default async function RootLayout({ children }: { children: React.ReactNo
     );
 
     return (
-    <html lang="en" className="h-full">
-        <body suppressHydrationWarning className={`${inter.className} antialiased h-full flex flex-col`}>
-          <Navbar session={session}/>
-          <main className="flex-1 min-h-0">
-              <Providers>
-                  {children}
-              </Providers>
-              <Analytics/>
-          </main>
-      </body>
-    </html>
-);
+        <html lang="en" className="h-full">
+            <body suppressHydrationWarning className={`${inter.className} antialiased h-full flex flex-col`}>
+                <Navbar session={session}/>
+                <main className="flex-1 min-h-0">
+                    <Providers>
+                        {children}
+                    </Providers>
+                    <Analytics/>
+                </main>
+            </body>
+        </html>
+    );
 }
 
 
